1fd0dedd6bf9d0578069993fd44deb91f1ae442b
Ronan <botrel.ronan29@gmail.com>
2026-05-22 22:21:53 +0200

    ADD: refont

--- tournament-of-legends/scenes/main_character.gd
+++ tournament-of-legends/scenes/main_character.gd
@@ -18,7 +18,7 @@ func _physics_process(delta: float) -> void:
 		sprite_2d.animation = "jump"
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_select") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
