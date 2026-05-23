693b22890d4636785de20b0a37415fde5c7460ad
thibaut <thibaut.hien@epitech.eu>
2026-05-23 10:48:26 +0200

    push def arthuria

--- tournament-of-legends/Attack/defAttack.gd
+++ tournament-of-legends/Attack/defAttack.gd
@@ -1,2 +1,5 @@
 class_name def_attack
 extends BaseAttack
+
+func _perform(character: BaseCharacter) -> void :
+	pass
--- tournament-of-legends/characters/base_characyer.gd
+++ tournament-of-legends/characters/base_characyer.gd
@@ -7,9 +7,9 @@ const JUMP_VELOCITY = -500.0
 var sprite_2d: AnimatedSprite2D
 var attacks: Array[BaseAttack] = []
 var hp: int
-var lvl_multiplicator: float
-var defense_multiplicator: float
-var jauge_spe: int
+var lvl_multiplicator: float = 1
+var defense_multiplicator: float = 1
+var jauge_spe: int = 500
 var is_in_action := false
 func setup_attacks() -> void:
 	pass
@@ -21,36 +21,35 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	_handle_movement(delta)
 	_handle_attack_inputs()
+	_update_action_state()
 
 
 func _handle_movement(delta: float) -> void:
-	if not is_in_action:
-		if not is_on_floor():
-			sprite_2d.animation = "jump"
-		elif (velocity.x > 1 || velocity.x < -1):
-			sprite_2d.animation = "run"
-		else:
-			sprite_2d.animation = "idle"
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	if is_in_action:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		move_and_slide()
+		return  # ← stop, pas de saut ni déplacement
+	if not is_on_floor():
+		sprite_2d.animation = "jump"
+	elif abs(velocity.x) > 1:
+		sprite_2d.animation = "run"
+	else:
+		sprite_2d.animation = "idle"
+
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		print("test")
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	
+
 	move_and_slide()
-	
+
 	var isLeft = velocity.x < 0
 	sprite_2d.flip_h = isLeft
 func charge_gauge(amount: int) -> void:
@@ -81,3 +80,9 @@ func _heal(heal: int) -> void:
 func _process(delta: float) -> void:
 	for i in attacks.size():
 		attacks[i].tick(delta)
+
+func _update_action_state() -> void:
+	if not is_in_action:
+		return
+	var still_channelling = attacks.any(func(a): return a._decaying_channelling > 0)
+	is_in_action = still_channelling
